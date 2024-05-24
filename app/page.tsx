5638d38ab060f08e1378fa73f9eb748667e908c2
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-05-24 19:00:44 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <motion.div style={{ display: "flex", gap: "30px" }} layout>
+        <AnimatePresence mode="sync">
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: "50rem" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              scale: 1,
+              transition: { delay: 0 },
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0,
+              transition: { duration: 0.4 },
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <h1>Node 1</h1>
+          </motion.div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: "50rem" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              scale: 1,
+              transition: { delay: 0.05 },
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0,
+              transition: { duration: 0.4 },
+            }}
+          >
+            <h1>Node 2</h1>
+          </motion.div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: "50rem" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              scale: 1,
+              transition: { delay: 0.1 },
+            }}
+            exit={{
+              opacity: 0,
+              scale: 0,
+              transition: { duration: 0.4 },
+            }}
+          >
+            <h1>Node 3</h1>
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
     </main>
   );
 }
